3d43361d595e5bc743d42c8bca288e23315deb25
ADeGobbis <49430541+ADeGobbis@users.noreply.github.com>
2022-01-10 14:23:44 +0100

    Added plot for histories

--- tf_lassonet/graphics.py
+++ tf_lassonet/graphics.py
@@ -48,3 +48,68 @@ def feature_importance_histogram(
     ax.set_ylabel("Importance")
     ax.set_xlabel("Feature")
     return ax
+
+def plot_history(*args: List[HistoryItem], figsize = (8, 8), title = None, legend = None):
+
+    """
+    Plot the evolution of the model on the path, namely:
+    - lambda
+    - number of selected variables
+    - score
+
+
+    Parameters
+    ==========
+    path: the result of a LassoPath.fit()
+    """
+    n_selected = []
+    score = []
+    lambda_ = []
+    for path in args:
+        n_cur = []
+        score_cur = []
+        lambda_cur = []
+        for history in path:
+            n_cur.append(history.n_selected_features[0])
+            score_cur.append(history.val_loss)
+            lambda_cur.append(history.lambda_)
+        n_selected.append(n_cur)
+        score.append(score_cur)
+        lambda_.append(lambda_cur)
+
+
+    plt.figure(figsize=figsize)
+
+    plt.subplot(311)
+    plt.grid(True)
+    for x, y in zip(n_selected, score):
+        plt.plot(x, y, ".-")
+    plt.xlabel("number of selected features")
+    plt.ylabel("score")
+    if legend is not None:
+        plt.legend(legend, loc='center left', bbox_to_anchor=(1, 0.5))
+
+    plt.subplot(312)
+    plt.grid(True)
+    for x, y in zip(lambda_, score):
+        plt.plot(x, y, ".-")
+    plt.xlabel("lambda")
+    plt.xscale("log")
+    plt.ylabel("score")
+    if legend is not None:
+        plt.legend(legend, loc='center left', bbox_to_anchor=(1, 0.5))
+
+    plt.subplot(313)
+    plt.grid(True)
+    for x, y in zip(lambda_, n_selected):
+        plt.plot(x, y, ".-")
+    plt.xlabel("lambda")
+    plt.xscale("log")
+    plt.ylabel("number of selected features")
+    if legend is not None:
+        plt.legend(legend, loc='center left', bbox_to_anchor=(1, 0.5))
+
+    if title is not None:
+        plt.title(title)
+
+    plt.tight_layout()
